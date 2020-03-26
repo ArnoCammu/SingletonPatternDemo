96a481f4d348220fd49212fd923e64a6aaf1c2de
arno cammu <arnocammu@hotmail.com>
2020-03-26 15:40:57 +0100

    Add project files.

---
+++ SingletonPatternDemo/EmployeeServices.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPatternDemo
+{
+    public class EmployeeServices // Dit is de Singleton class,niet static
+
+    {
+        private static EmployeeServices instance; // private field of employeeservices type
+
+        private List<Employee> employeeList = null; // Dit moet op null staan!
+
+
+        private static readonly object _lock = new object(); // _lock is GMP
+        // Dit is voor de threading. we willen dat methodes maar door max 1 persoon wordt uitgevoerd ipv simultaan
+
+        private EmployeeServices() // private ctor (normaal public)
+        {
+            if (employeeList == null) // controle of er maar 1 instantie is! Max 1!
+            {
+                employeeList = new List<Employee>();
+            }
+        }
+        // Dus bij singleton maar max 1 instantie
+
+        // non-thread safe
+        //public static EmployeeServices Instance()// Dit noemt altijd Instance!
+        //{
+        //    
+        //        instance = new EmployeeServices();
+        //    
+        //    return instance; // Dus als je instance oproept (via Instance methode) dan maak je de list.
+        //}
+
+        // thread safe --> dit moet bij de critical session, dit is de critical session
+        public static EmployeeServices Instance()// Dit noemt altijd Instance!
+        {
+            if (instance == null) // nu 100% zeker dat dit thread safe is. 100% goed. Dit is GMP maar moet niet van Kenan
+            {
+                lock (_lock) // Dit maakt het redelijk thread safe (99% goed)
+                {
+                    if (instance == null) // controle hier ook
+                    {
+                        instance = new EmployeeServices();
+                    }
+                }
+            }
+            return instance; // Dus als je instance oproept (via Instance methode) dan maak je de list.
+        }
+
+        public void AddEmployees(Employee employee)
+        {
+            employeeList.Add(employee);
+        }
+        public List<Employee> GetEmployeesByFirstName(string firstName)
+        {
+            return employeeList.FindAll(x => x.FirstName.Contains(firstName));
+        }
+
+
+
+    }
+}
---
+++ SingletonPatternDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Static class heeft geen instanties. Maar static class is lastig. Voor opslaan van gegevens is dit slecht
+            // Deze static class wordt opgemaakt en blijft in het geheugen tot zolang de app loopt.
+            // Daarin heeft singleton een voordeel. Je hebt hier maar 1 instantie nodig ipv meerdere. Minder lastig voor het geheugen
+            // Dit is ook beter voor unit testing en wordt erg veel gebruikt bij loggers
+
+
+            Employee emp1 = new Employee();
+            emp1.FirstName = "Kenan";
+            emp1.LastName = "Kurda";
+            Employee emp2 = new Employee();
+            emp2.FirstName = "Jan";
+            emp2.LastName = "Martens";
+            Employee emp3 = new Employee();
+            emp3.FirstName = "Kevin";
+            emp3.LastName = "Burghaus";
+            Employee emp4 = new Employee();
+            emp4.FirstName = "Arek";
+            emp4.LastName = "IetsPools";
+            Employee emp5 = new Employee();
+            emp5.FirstName = "Bol";
+            emp5.LastName = "Bauwens";
+
+            EmployeeServices empServices = EmployeeServices.Instance(); // Een nieuwe class gemaakt
+            // Deze kun je meerdere keren aanroepen maar hij maakt toch maar eentje aan!
+
+            empServices.AddEmployees(emp1);
+            empServices.AddEmployees(emp2);
+            empServices.AddEmployees(emp3);
+            empServices.AddEmployees(emp4);
+            empServices.AddEmployees(emp5);
+
+            List<Employee> LijstMetNaam = empServices.GetEmployeesByFirstName("Ke"); // ik kan ook var gebruiken ipv de lijst
+
+            foreach (var item in LijstMetNaam)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName}"); // 2 namen met "Ke" erin!
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
